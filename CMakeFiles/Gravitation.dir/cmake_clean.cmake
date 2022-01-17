01f4eb6b410ae80177fc24674047194d6d7f079f
itspacchu <37984032+itspacchu@users.noreply.github.com>
2022-01-17 23:36:18 +0530

    init

---
+++ CMakeFiles/Gravitation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gravitation.dir/main.cpp.o"
+  "CMakeFiles/Gravitation.dir/main.cpp.o.d"
+  "Gravitation"
+  "Gravitation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gravitation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
